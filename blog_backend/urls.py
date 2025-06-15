0abebfcb01135e9e4f4ad52ae5e548ca35710ae8
kodurisaiganesh <146118669+kodurisaiganesh@users.noreply.github.com>
2025-06-15 15:43:42 +0530

    first commit

--- blog_backend/urls.py
+++ blog_backend/urls.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from blog.views import MyTokenObtainPairView  # <-- custom view
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.http import JsonResponse  # <-- added import
+
+def root_view(request):
+    return JsonResponse({"message": "Welcome to the Blog API", "status": "ok"})
 
 urlpatterns = [
+    path('', root_view),  # <-- added this line
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),  # This gives /api/blogs/
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
